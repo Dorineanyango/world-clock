cc4563954c1ec6ba62c4197669119e05dc90aeae
Dorine <anyangodorine474@gmail.com>
2024-09-23 16:34:10 +0300

    Added functionality

---
+++ javascript/script.js
@@ -0,0 +1,23 @@
+function updateTime() {
+    let nairobiElement = document.querySelector("#nairobi");
+    let nairobiDateElement = nairobiElement.querySelector(".date");
+    let nairobiTimeElement = nairobiElement.querySelector(".time");
+    let nairobiTime = moment().tz("Africa/Nairobi");
+
+
+    nairobiDateElement.innerHTML = nairobiTime.format("MMMM Do, YYYY");
+    nairobiTimeElement.innerHTML = nairobiTime.format('h:mm:ss[<small>]A[</small>]');
+
+
+    let parisElement = document.querySelector("#paris");
+    let parisDateElement = parisElement.querySelector(".date");
+    let parisTimeElement = parisElement.querySelector(".time");
+    let parisTime = moment().tz("Europe/Paris");
+
+
+    parisDateElement.innerHTML = parisTime.format("MMMM Do, YYYY");
+    parisTimeElement.innerHTML = parisTime.format('h:mm:ss[<small>]A[</small>]');
+}
+
+updateTime();
+setInterval(updateTime, 1000);
